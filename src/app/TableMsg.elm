ccc4bae656279a19b8094ee5ccde93fe3661165b
Julian Leviston <julian.leviston@blake.com.au>
2017-03-18 18:17:08 +1100

    Flesh out table rendering

---
+++ src/app/TableMsg.elm
@@ -0,0 +1,13 @@
+module TableMsg exposing (Msg(..))
+
+{-|
+
+   This module has the Table Msg type
+   So that it can be accessed from several places
+
+-}
+
+
+type Msg
+    = CallLiar
+    | MakeBid
